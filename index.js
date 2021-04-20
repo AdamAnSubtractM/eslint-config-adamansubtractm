2f8e1fe455c917daebed5b5f738c15e411d13b8b
Adam Knee <adam.l.knee@gmail.com>
2021-04-19 22:51:15 -0400

    Update config and bump version.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const eslintrc = require('./.eslintrc.js');
-const prettierrc = require('./.prettierrc');
+const prettierrc = require('./.prettierrc.js');
 const prettierignore = require('./.prettierignore');
 
 module.exports = { eslintrc, prettierrc, prettierignore };
